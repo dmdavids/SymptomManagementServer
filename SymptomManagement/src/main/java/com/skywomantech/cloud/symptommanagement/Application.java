7c48b4797c5bc1023373baf8d915d19a69590cf7
Daun Davids <daundavids@gmail.com>
2014-11-04 14:23:32 -0700

    Working with app for login with oauth2!

--- SymptomManagement/src/main/java/com/skywomantech/cloud/symptommanagement/Application.java
+++ SymptomManagement/src/main/java/com/skywomantech/cloud/symptommanagement/Application.java
@@ -1,20 +1,21 @@
 package com.skywomantech.cloud.symptommanagement;
 
+import org.skywomantech.cloud.symptommanagement.auth.OAuth2SecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @EnableAutoConfiguration
 @EnableMongoRepositories
 @EnableWebMvc
 @Configuration
 @ComponentScan
+@Import(OAuth2SecurityConfiguration.class)
 public class Application extends RepositoryRestMvcConfiguration {
 	
 	public static void main(String[] args) {
